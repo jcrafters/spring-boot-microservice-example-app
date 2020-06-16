9cfa799f52761a584e1f2d6b97ca5c4e9bed2fe7
Michał Gruszczyński <michal.gruszczynski@cherrypick.pl>
2020-06-16 21:15:28 +0200

    Przejście na cloud + Usługa Eureka

---
+++ jc-stock-market-parent/jc-stock-market-eureka-app/src/main/java/com/jcrafters/stockmarket/eureka/StockMarketEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.jcrafters.stockmarket.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class StockMarketEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockMarketEurekaApplication.class, args);
+    }
+
+}
